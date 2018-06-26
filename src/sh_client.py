a210bb71f0ae345c1473e963a6078043e46aa024
András Hegedüs <ahegedus@hortonworks.com>
2018-06-26 13:07:01 +0200

    init

--- src/sh_client.py
+++ src/sh_client.py
@@ -23,7 +23,7 @@ class HstClient(object):
     def capture_bundle(self):
         try:
             print("Capturing smartsense bundle...")
-            check_output(["sudo " + self.client_path,"capture"])
+            check_output(["sudo",self.client_path,"capture"])
             print("Smartsense boundle captured")
         except CalledProcessError as e:
             raise HstError("Smartsense capture has been failed, please check hst logs for details")
@@ -48,12 +48,12 @@ class AmbariServer(object):
 
     def stop(self):
         try:
-            check_output(["sudo " + self.client_path,"stop"])
+            check_output(["sudo",self.client_path,"stop"])
         except CalledProcessError as e:
             raise AmbariServerError("Some problem have been occurred during stopping ambari-server: {0}".format(e.message))
 
     def start(self):
         try:
-            check_output(["sudo " + self.client_path,"start"])
+            check_output(["sudo",self.client_path,"start"])
         except CalledProcessError as e:
             raise AmbariServerError("Some problem have been occurred during starting ambari-server: {0}".format(e.message))
