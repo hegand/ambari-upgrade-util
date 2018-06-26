1eb24388f53a6a14130b3c031334457d6725e165
András Hegedüs <ahegedus@hortonworks.com>
2018-06-26 13:29:00 +0200

    init

--- src/sh_client.py
+++ src/sh_client.py
@@ -50,14 +50,25 @@ class AmbariServer(object):
         try:
             print("Stopping ambari-server...")
             check_output(["sudo",self.client_path,"stop"])
+            if self.check_if_running():
+                raise AmbariServerError("Stopping ambari-server was not successful, please check")
             print("Ambari server has been stopped successfully")
         except CalledProcessError as e:
-            raise AmbariServerError("Some problem have been occurred during stopping ambari-server: {0}".format(e.message))
+            raise AmbariServerError("Some problem have been occurred during stopping ambari-server")
 
     def start(self):
         try:
             print("Starting ambari-server...")
             check_output(["sudo",self.client_path,"start"])
+            if not self.check_if_running():
+                raise AmbariServerError("Starting ambari-server was not successful, please check")
             print("Ambari server has been started successfully")
         except CalledProcessError as e:
-            raise AmbariServerError("Some problem have been occurred during starting ambari-server: {0}".format(e.message))
+            raise AmbariServerError("Some problem have been occurred during starting ambari-server")
+
+    def check_if_running(self):
+        try:
+            check_output(["sudo","cat","/var/run/ambari-server/ambari-server.pid"])
+            return True
+        except CalledProcessError as e:
+            return False
