a2190414a7b1337f11512f9da79dcf7c25e9a0a0
András Hegedüs <ahegedus@hortonworks.com>
2018-07-03 08:59:29 +0200

    init

--- src/sh_client.py
+++ src/sh_client.py
@@ -2,7 +2,7 @@ from subprocess import check_output, CalledProcessError
 from time import sleep
 from datetime import datetime
 from os import path
-import shlex
+import copy, shlex
 
 from sh_error import HstError,AmbariServerError,SshError,ShError
 
@@ -10,7 +10,7 @@ from sh_error import HstError,AmbariServerError,SshError,ShError
 class ShClient(object):
     def __init__(self):
         self.checking()
-        self.sudo = self.checking_sudo()
+        self.is_sudo = self.checking_sudo()
 
     @staticmethod
     def checking():
@@ -22,30 +22,29 @@ class ShClient(object):
     @staticmethod
     def checking_sudo():
         try:
-            return check_output(["sudo","whoami"]).split("\n")[:-1][0] == 'root'
+            return check_output("sudo whoami").split("\n")[0] == 'root'
         except CalledProcessError as e:
             return False
 
     @staticmethod
     def construct_command(command, runasuser=None):
-        if type(command) is str:
-            _command = [command]
-        elif type(command) is list:
-            _command = command
-        else:
+        if type(command) is not str:
             raise ShError("Command must be string or list")
+        _command = copy.deepcopy(command)
         if runasuser == "root":
-            _command = ["sudo"] + _command
+            _command = "sudo {0}".format(_command)
         elif runasuser is not None:
-            _command = ["sudo","-u",runasuser] + _command
+            _command = "sudo -u {0} {1}".format(runasuser, _command)
         return _command
 
-    def run(self,command,runasuser=None,shell=False):
-        if runasuser is not None and not self.sudo:
+    def run(self, command, runasuser=None, shell=False):
+        if runasuser is not None and not self.is_sudo:
             raise ShError("The current user does not have sudo right")
-        _command = self.construct_command(command,runasuser)
+        _command = self.construct_command(command, runasuser)
+        if shell:
+            _command = shlex.split(_command)
         try:
-            return check_output(_command,shell=shell)
+            return check_output(_command, shell=shell)
         except CalledProcessError as e:
             raise ShError(e.message)
 
@@ -56,20 +55,20 @@ class SshClient(ShClient):
         self.hostname = hostname
 
     def construct_command(self, command, runasuser=None):
-        return self.base_command + super(SshClient,self).construct_command(command,runasuser)
+        return self.base_command + super(SshClient,self).construct_command(command, runasuser)
 
     @staticmethod
     def construct_ssh_base_command(hostname, remoteuser, runaslocaluser):
-        command = ["ssh"]
+        command = "ssh"
         if remoteuser is not None:
-            command.append("{0}@{1}".format(remoteuser,hostname))
+            command = "{0} {1}@{2}".format(command, remoteuser, hostname)
         else:
-            command.append(hostname)
+            command = "{0} {1}".format(command, hostname)
         if runaslocaluser == "root":
-            command = ["sudo"] + command
+            command = "sudo {0}".format(command)
         elif runaslocaluser is not None:
-            command = ["sudo","-u", runaslocaluser] + command
-        return command + ["TERM=dumb"]
+            command = "sudo -u {0} {1}".format(runaslocaluser, command)
+        return "{0} TERM=dumb".format(command)
 
 
 class HstClient(object):
@@ -163,7 +162,7 @@ class BackupClient(object):
     def __init__(self, sh_client, case_number, backup_base_dir):
         self.sh_client = sh_client
         self.case_number = case_number
-        self.backup_dir = path.expanduser("{0}case{0}/".format(backup_base_dir,self.case_number))
+        self.backup_dir = path.expanduser("{0}{1}case{2}/".format(backup_base_dir, "/" if not backup_base_dir.endswith("/") else "", self.case_number))
         self.holland_client = HollandClient(self.sh_client)
 
     def create_backup_dir(self):
@@ -174,10 +173,9 @@ class BackupClient(object):
 
     def create_backup(self):
         tms = datetime.now().strftime('%Y-%m-%d_%H:%M')
-        _command = shlex.split("ls -la /etc/ambari-* /etc/smartsense* " +
-                               "/etc/hst /etc/yum.repos.d /usr/hdp/current/zeppelin-server/notebook " +
-                               "/etc/zeppelin /var/lib/ambari-server/ambari-env.sh " +
-                               "| xargs readlink -e")
+        _command = ("readlink -e /etc/ambari-* /etc/smartsense* " +
+                   "/etc/hst /etc/yum.repos.d /usr/hdp/current/zeppelin-server/notebook " +
+                   "/etc/zeppelin /var/lib/ambari-server/ambari-env.sh")
         files = " ".join(self.sh_client.run(_command, "root", True).split("\n")[:-1])
         self.create_backup_dir()
         _tar_command = shlex.split("tar --ignore-failed-read -cvzf {0}backup-files_{1}.tar.gz {2}"\
@@ -195,7 +193,7 @@ class HollandClient(object):
         self.sh_client.run(self.exec_path,["backup",backupset],"root")
 
     def get_base_backup_dir(self,backupset="default"):
-        return "{0}/{1}".format(self.sh_client.run(["/bin/grep", "-r", "backup_dir", "/etc/holland/holland.conf"], "root").split(" = ")[1].split("\n")[0],backupset)
+        return "{0}/{1}".format(self.sh_client.run("/bin/grep -r 'backup_dir' /etc/holland/holland.conf", "root").split(" = ")[1].split("\n")[0],backupset)
 
     def get_newest_backup_dir(self,backupset="default"):
         return "{0}newest/".format(self.get_base_backup_dir(backupset))
