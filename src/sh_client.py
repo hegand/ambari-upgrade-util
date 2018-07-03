8189a2dff8377a1ce557273104dde1805f22f404
András Hegedüs <ahegedus@hortonworks.com>
2018-07-03 09:24:18 +0200

    init

--- src/sh_client.py
+++ src/sh_client.py
@@ -167,7 +167,7 @@ class BackupClient(object):
 
     def create_backup_dir(self):
         try:
-            self.sh_client.run("mkdir {0}".format(self.backup_dir))
+            self.sh_client.run("mkdir {0}".format(self.backup_dir),"root")
         except CalledProcessError as e:
             raise ShError(e.message)
 
